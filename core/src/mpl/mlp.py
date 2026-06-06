512c10ba627f0cc2b6181b772f48e13da6120021
José Melo <jos.ryan.melo@gmail.com>
2026-06-06 16:58:26 -0300

    feat: add basic mlp

---
+++ core/src/mpl/mlp.py
@@ -0,0 +1,45 @@
+from sklearn.   datasets import load_iris
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import RobustScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+
+dataset = load_iris()
+
+"""
+O scaler é o 
+"""
+scaler = RobustScaler()
+
+X = dataset['data']
+Y = dataset['target']
+target_names = dataset['target_names']
+feature_names = dataset['feature_names']
+
+
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X, 
+    Y, 
+    test_size=0.3)
+
+
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.fit_transform(X_test)
+
+mlp = MLPClassifier(
+    hidden_layer_sizes=(10,),
+    activation='relu',
+    solver='sgd',
+    alpha=0.0001,
+    max_iter=10
+)
+
+mlp.fit(X=X_train, y=Y_train)
+
+Y_predictions = mlp.predict(X_test)
+
+accuracy = accuracy_score(Y_test, Y_predictions)
+
+
+print(accuracy)
