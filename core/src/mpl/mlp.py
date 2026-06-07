880e366f122b95255b6f52c4f4d76aae824d482f
Kayo Emanuel Salvino do Nascimento <kayo.emanuel.salvino.do.nascimento@ccc.ufcg.edu.br>
2026-06-06 22:54:39 -0300

    Testes iniciais do random_search.

--- core/src/mpl/mlp.py
+++ core/src/mpl/mlp.py
@@ -3,11 +3,11 @@ import random_search
 from sklearn.neural_network import MLPClassifier
 
 
-random_search_results: random_search.SearchResult = random_search.get_params()
+random_search_results: random_search.GetParamsReturn = random_search.get_params()
 random_search_best_params: random_search.MLPParams = random_search_results["best_params"]
 
 mlp = MLPClassifier(
-    hidden_layer_sizes=random_search_best_params['hidden_neurons_size'],
+    hidden_layer_sizes=(random_search_best_params['hidden_neurons_size'],),
     activation=random_search_best_params['activation'],
     alpha=random_search_best_params['alpha'],
     solver=random_search_best_params['solver'],
